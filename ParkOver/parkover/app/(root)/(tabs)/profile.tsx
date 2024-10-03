d766e7c29c47819af47af464c10c0f7d08141c3e
Amjadkp <amjuamjadkp11@gmail.com>
2024-10-03 22:33:09 +0530

    something is done

---
+++ ParkOver/parkover/app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile =() => {
+    return(
+        <SafeAreaView>
+            <Text>
+                Porfile
+            </Text>
+        </SafeAreaView>
+    )
+
+}
+
+export default Profile;

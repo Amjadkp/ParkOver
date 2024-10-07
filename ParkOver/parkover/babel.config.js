6bba2c09e6629ab72cfb4597ee721a971c2aa511
Amjadkp <amjuamjadkp11@gmail.com>
2024-10-07 21:18:24 +0530

    onboarding_pages_trial

--- ParkOver/parkover/babel.config.js
+++ ParkOver/parkover/babel.config.js
@@ -1,7 +1,25 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: ["nativewind/babel"],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            "@assets": "./ParkOver/parkover/assets", // Add your alias for assets
+          },
+        },
+      ],
+    ],
   };
 };
